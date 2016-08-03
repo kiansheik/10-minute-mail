e61bc4c25447b936dc62fe4796ea6b6d3241a1c3
Kian Sheik <kiansheik3128@gmail.com>
2016-08-02 23:01:06 -0700

    Added class so it can be used as library, if an
    yone would ever want such a thing

--- 10minmail.py
+++ 10minmail.py
@@ -3,6 +3,7 @@
 
 #stdlib
 import os, sys
+from json import loads
 from subprocess import call
 from datetime import datetime
 from email.utils import parseaddr
@@ -11,25 +12,37 @@ from email.utils import parseaddr
 ## sudo pip install --user websocket-client
 from websocket import create_connection
 
-print("PID: {0}\nIf you can't quite, run 'kill {0}'\n".format(os.getpid()))
+class mailbox(object):
+	"""docstring for mailbox"""
+	def __init__(self):
+		super(mailbox, self).__init__()
+		self.ws = create_connection("wss://dropmail.me/websocket")
+		self.email = self.ws.recv()[1:].split(":")[0]
+		self.ws.recv()
 
-def main(ws):
-	first = True
+	def next(self):
+		"""Returns the next message. Blocking."""
+		return self.ws.recv()		
+
+def main(box):
+	call(["echo '{0}' | pbcopy".format(box.email)], shell=True)
+	print (box.email+" was copied to clipboard")
 	while True:
-		result =  ws.recv()
-		email = result[1:].split(":")[0]
-		if first and'@' in parseaddr(email)[1]:
-			call(["echo '{0}' | pbcopy".format(email)], shell=True)
-			print (email+" was copied to clipboard")
-			first = False
-		else:
+		result =  box.next()
+		try:
+			print("Recieved following at {0}".format( datetime.now()))
+			for k in loads(result[1:]).items():
+				print("\t%s: %s" % k)
+		except:
 			print("Recieved:{1} {0}\n".format(result, datetime.now()))
+
 	ws.close()
 
 if __name__ == '__main__':
+	print("PID: {0}\nIf you can't quite, run 'kill {0}'\n".format(os.getpid()))
 	try:
-		ws = create_connection("wss://dropmail.me/websocket")
-		main(ws)
+		box = mailbox()
+		main(box)
 	except KeyboardInterrupt:
 		ws.close()
 		sys.exit(0)
